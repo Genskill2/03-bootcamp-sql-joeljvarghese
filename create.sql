63f2b0c11e3a37388abbf6af8602b5379b82db4f
Joel Varghese <joeljosevarghese1@gmail.com>
2021-06-17 16:53:26 +0300

    Debugging

--- create.sql
+++ create.sql
@@ -1,28 +1,22 @@
 CREATE TABLE  publisher (
-id int NOT NULL ,
-name varchar(255),
-country varchar(255),
-PRIMARY KEY (id)
+id serial PRIMARY KEY ,
+name varchar(255) NOT NULL,
+country varchar(255)
 );
 
 CREATE TABLE  books (
-id int ,
-title varchar(255),
-publisher int,
-PRIMARY KEY (id),
-FOREIGN KEY (publisher) REFERENCES publisher(id)
+id int PRIMARY KEY ,
+title varchar(255) NOT NULL,
+publisher serial REFERENCES publisher(id)
 );
 
-CREATE TABLE  subjects(
-id int ,
-name varchar(255),
-PRIMARY KEY (id)
+CREATE TABLE subjects (
+id serial PRIMARY KEY,
+name varchar(255) NOT NULL 
 );
 
 
 CREATE TABLE  books_subjects(
-book varchar(255),
-subject varchar(255),
-FOREIGN KEY (book) REFERENCES books(id),
-FOREIGN KEY (subject) REFERENCES books(id)
+book varchar(255) REFERENCES books(id),
+subject varchar(255) REFERENCES subjects(id)
 );
